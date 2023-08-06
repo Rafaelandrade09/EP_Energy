4510d468623e26d7ebbd6eeb52f23dada58bd987
Rafael Andrade <rafael.andrade@orram.com.br>
2023-08-06 12:07:19 -0300

    estruturei as paginas NossasSolucoes

--- src/pages/Desktop26.tsx
+++ src/pages/Desktop26.tsx
@@ -1,45 +1,55 @@
 import { FunctionComponent } from "react";
+import BarraNav from "../componentes/PagPrincipal/BarraNav";
+import Rodape from "../componentes/PagPrincipal/Rodape";
+import MargensLaterais from "../componentes/PagPrincipal/MargensLaterais";
 
 const Desktop26: FunctionComponent = () => {
   return (
-    <div className="relative bg-gray-500 w-full h-[90.19rem] overflow-hidden text-center text-[1.56rem] text-secondary font-saira-semicondensed">
-      <div className="absolute top-[4.25rem] left-[4.25rem] bg-gray-600 w-[79.75rem] h-[136.56rem]" />
-      <div className="absolute top-[0.06rem] left-[0rem] bg-gray-900 w-[87.69rem] h-[4.75rem]" />
-      <div className="absolute top-[1.44rem] left-[4rem] inline-block w-[4.19rem]">
-        HOME
-      </div>
-      <img
-        className="absolute top-[0rem] left-[32.13rem] w-[21.13rem] h-[8.13rem] object-cover"
-        alt=""
-        src="/epenergyremovebgpreview-2@2x.png"
-      />
-      <div className="absolute top-[6rem] left-[89.31rem] inline-block w-[5.5rem]">
-        SERVIÇOS
-      </div>
-      <div className="absolute top-[90.13rem] left-[88.31rem] text-[3.75rem] font-semibold font-playfair-display text-dimgray text-left inline-block w-[1.94rem] h-[3.56rem]">{`>`}</div>
-      <div className="absolute top-[4.81rem] left-[83.75rem] bg-whitesmoke-400 w-[3.94rem] h-[204.19rem]" />
-      <div className="absolute top-[4.81rem] left-[0rem] bg-whitesmoke-400 w-[4rem] h-[204.19rem]" />
-      <div className="absolute top-[1.44rem] left-[19.75rem] inline-block w-[6.63rem]">
-        CONTATO
-      </div>
-      <div className="absolute top-[0.06rem] left-[67.81rem] bg-darkslategray-200 w-[19.88rem] h-[4.75rem]" />
-      <div className="absolute top-[1.38rem] left-[57.56rem] inline-block w-[6rem]">
-        SOBRE
-      </div>
-      <div className="absolute top-[1.38rem] left-[70.31rem] inline-block w-[13.44rem]">
-        NOSSAS SOLUÇÕES
-      </div>
-      <b className="absolute top-[6.88rem] left-[40.19rem] text-[2.5rem] inline-block font-playfair-display text-gray-700 text-left w-[35.13rem]">
+    <div className="relative bg-gray-500 w-full h-[95.19rem] overflow-hidden text-center text-[1.56rem] text-secondary font-saira-semicondensed">
+    
+      <BarraNav/>
+
+
+      <b className="absolute top-[6.88rem] left-[40.19rem] text-[2.5rem] inline-block font-arima-madurai-display text-gray-700 text-left w-[35.13rem]" style={{color: '#0F2631'}}>
         Cubículo modular c/disjuntor extraível até 2500A
       </b>
+
+      {/* IMAGEM DO PRODUTO */}
       <img
-        className="absolute top-[7.75rem] left-[6.5rem] w-[32.19rem] h-[43.75rem] object-cover"
+        className="absolute top-[7.75rem] left-[6.5rem] w-[32.19rem] h-[44.75rem] object-cover"
         alt=""
         src="/image-8@2x.png"
       />
-      <div className="absolute top-[31.81rem] left-[40.19rem] bg-whitesmoke-300 w-[40.31rem] h-[19.69rem]" />
-      <div className="absolute top-[35.69rem] left-[42.19rem] text-[inherit] font-inherit text-black text-left inline-block w-[29.31rem]">
+      
+
+
+      <div className="absolute top-[15.19rem] left-[40.5rem] text-[1.10rem] font-arima-madurai text-black text-left inline-block w-[40rem]">
+        <p className="m-0">
+          {" "}
+          Cubículos modelares tipo metal-clad e disjuntor extraível até 2500A,
+          31,5kA/1s em 17,5kV para uso abrigado e ao tempo, certificado conforme
+          NBR-IEC-62271-200, uma linha especial e de alta performance.
+        </p>
+        <p className="m-0">
+          {" "}
+          Possui ensaios realizados de acordo com a última revisão das normas e
+          aprovadas por laboratórios de organizações independentes de
+          reconhecimento internacional, IEE-USP e SVEPPI-ACCREDIA.
+        </p>
+      </div>
+
+
+      {/* CARACTERISTICAS */}
+      <div className="absolute top-[26.81rem] left-[40.5rem] w-[35.31rem] h-[25.69rem]"  style={{backgroundColor: '#0F2631'}}/>
+      
+      <div className="absolute top-[27.19rem] left-[42.63rem] text-black text-left inline-block w-[16.25rem] text-[1.38rem] font-playfair-display" style={{color: 'white'}}>
+        <p className="m-0">&nbsp;</p>
+          <span className="font-semibold">Características Tecnicas:</span>
+      </div>
+
+      <div className="absolute top-[30.69rem] left-[44.19rem] text-[1.10rem] font-arima-madurai text-black text-left inline-block w-[29.31rem]" style={{color: 'white'}}>
         <ul className="m-0 pl-[1.31rem]">
+        <p className="m-0">&nbsp;</p>
           <li className="mb-[false]">{`Cubículo 17,5kV; `}</li>
           <li className="mb-[false]">
             Resistente ao arco interno 31,5kA com disjuntor extraível;-
@@ -57,40 +67,23 @@ const Desktop26: FunctionComponent = () => {
           </li>
         </ul>
       </div>
-      <div className="absolute top-[18.19rem] left-[40.5rem] text-[1rem] font-arima-madurai text-black text-left inline-block w-[41rem]">
-        <p className="m-0">
-          {" "}
-          Cubículos modelares tipo metal-clad e disjuntor extraível até 2500A,
-          31,5kA/1s em 17,5kV para uso abrigado e ao tempo, certificado conforme
-          NBR-IEC-62271-200, uma linha especial e de alta performance.
-        </p>
-        <p className="m-0">
-          {" "}
-          Possui ensaios realizados de acordo com a última revisão das normas e
-          aprovadas por laboratórios de organizações independentes de
-          reconhecimento internacional, IEE-USP e SVEPPI-ACCREDIA.
-        </p>
-      </div>
-      <div className="absolute top-[32.19rem] left-[42.63rem] text-black text-left inline-block w-[14.25rem] text-[1rem] font-playfair-display">
-        <p className="m-0">&nbsp;</p>
-        <p className="m-0 text-[1.25rem]">
-          <span className="font-semibold">Características</span>
-          <span className="text-[1rem] font-playfair-display">{` `}</span>
-          <span className="text-[1.25rem] font-semibold font-playfair-display">
-            técnicas
-          </span>
-          <span className="text-[1rem] font-playfair-display">:</span>
-        </p>
-      </div>
-      <b className="absolute top-[54.06rem] left-[38.44rem] text-[1.38rem] inline-block font-playfair-display text-black text-left w-[8.44rem]">
+
+      
+
+
+
+
+      <b className="absolute top-[59.06rem] left-[38.44rem] text-[1.38rem] inline-block font-playfair-display text-black text-left w-[8.44rem]">
         Descrição
       </b>
       <img
-        className="absolute top-[57.38rem] left-[6.5rem] w-[74rem] h-[0.12rem]"
+        className="absolute top-[58.38rem] left-[6.5rem] w-[74rem] h-[0.12rem]"
         alt=""
         src="/line-9.svg"
       />
-      <div className="absolute top-[58.75rem] left-[6.5rem] text-[1rem] font-arima-madurai text-black text-left inline-block w-[74rem]">
+
+
+      <div className="absolute top-[62.75rem] left-[7.5rem] text-[1.1rem] font-arima-madurai text-black text-left inline-block w-[70rem] ">
         {" "}
         A linha Maggiore de cubículos possui ensaios de tipo realizados de
         acordo com a última revisão das normas NBR-IEC 62271-200:2007 IEC
@@ -110,66 +103,10 @@ const Desktop26: FunctionComponent = () => {
         eletrolítico e percorre longitudinalmente todo o quadro, garantindo
         desta forma a máxima segurança para os operadores e para a instalação.
       </div>
-      <div className="absolute top-[76.13rem] left-[4.19rem] bg-gray-900 w-[79.81rem] h-[13.88rem]" />
-      <div className="absolute top-[79.63rem] left-[54.5rem] text-[1.25rem]">
-        HOME
-      </div>
-      <div className="absolute top-[79.63rem] left-[59.44rem] text-[1.25rem]">
-        CONTATO
-      </div>
-      <div className="absolute top-[79.63rem] left-[66rem] text-[1.25rem]">
-        SOBRE
-      </div>
-      <div className="absolute top-[79.63rem] left-[71.06rem] text-[1.25rem]">
-        NOSSA SOLUÇÕES
-      </div>
-      <div className="absolute top-[88.5rem] left-[33.44rem] text-[0.88rem] leading-[170%] font-be-vietnam-pro text-left">
-        © Copyright 2023 EP Energy Soluções em Energia
-      </div>
-      <div className="absolute top-[83.06rem] left-[33.25rem] text-[0.88rem] inline-block w-[19.06rem] h-[2.13rem]">
-        SAIBA MAIS EM NOSSAS REDES SOCIAIS
-      </div>
-      <div className="absolute top-[84.53rem] left-[34.59rem] box-border w-[16.44rem] h-[0.06rem] border-t-[1px] border-solid border-secondary" />
-      <img
-        className="absolute h-[1.98%] w-[2.64%] top-[94.41%] right-[52.6%] bottom-[3.61%] left-[44.76%] max-w-full overflow-hidden max-h-full"
-        alt=""
-        src="/vector5.svg"
-      />
-      <img
-        className="absolute h-[0.86%] w-[1.14%] top-[94.94%] right-[53.39%] bottom-[4.2%] left-[45.47%] max-w-full overflow-hidden max-h-full"
-        alt=""
-        src="/vector6.svg"
-      />
-      <img
-        className="absolute h-[1.98%] w-[1.35%] top-[94.41%] right-[49.82%] bottom-[3.61%] left-[48.82%] max-w-full overflow-hidden max-h-full"
-        alt=""
-        src="/vector7.svg"
-      />
-      <img
-        className="absolute h-[1.98%] w-[2.64%] top-[94.41%] right-[52.6%] bottom-[3.61%] left-[44.76%] max-w-full overflow-hidden max-h-full"
-        alt=""
-        src="/vector5.svg"
-      />
-      <img
-        className="absolute h-[0.86%] w-[1.14%] top-[94.94%] right-[53.39%] bottom-[4.2%] left-[45.47%] max-w-full overflow-hidden max-h-full"
-        alt=""
-        src="/vector6.svg"
-      />
-      <img
-        className="absolute h-[2.36%] w-[2.78%] top-[92.65%] right-[46.19%] bottom-[4.99%] left-[51.03%] max-w-full overflow-hidden max-h-full"
-        alt=""
-        src="/vector.svg"
-      />
-      <img
-        className="absolute h-[1.96%] w-[2.32%] top-[94.41%] right-[46.01%] bottom-[3.62%] left-[51.67%] max-w-full overflow-hidden max-h-full"
-        alt=""
-        src="/vector8.svg"
-      />
-      <img
-        className="absolute top-[75.19rem] left-[9.63rem] w-[19.38rem] h-[11.63rem] object-cover"
-        alt=""
-        src="/customcolor-textlogoname-transparent-background-1@2x.png"
-      />
+
+      <Rodape/>
+      <MargensLaterais/>
+      
     </div>
   );
 };
--- src/pages/NossasSolucoes.tsx
+++ src/pages/NossasSolucoes.tsx
@@ -1,99 +1,34 @@
 import { FunctionComponent } from "react";
+import BarraNav from "../componentes/PagPrincipal/BarraNav";
+import Rodape from "../componentes/PagPrincipal/Rodape";
+import MargensLaterais from "../componentes/PagPrincipal/MargensLaterais";
 
 const NossasSolucoes: FunctionComponent = () => {
   return (
     <div className="relative bg-gray-500 w-full h-[84.06rem] overflow-hidden text-center text-[1.56rem] text-secondary font-saira-semicondensed">
-      <div className="absolute top-[4.25rem] left-[4.25rem] bg-gray-600 w-[79.75rem] h-[79.25rem]" />
-      <div className="absolute top-[0.06rem] left-[0rem] bg-gray-900 w-[87.69rem] h-[4.75rem]" />
-      <div className="absolute top-[1.44rem] left-[4rem] inline-block w-[4.19rem]">
-        HOME
-      </div>
-      <img
-        className="absolute top-[0rem] left-[32.13rem] w-[21.13rem] h-[8.13rem] object-cover"
-        alt=""
-        src="/epenergyremovebgpreview-2@2x.png"
-      />
-      <div className="absolute top-[6rem] left-[89.31rem] inline-block w-[5.5rem]">
-        SERVIÇOS
-      </div>
-      <div className="absolute top-[1.13rem] left-[19.94rem] w-[39.06rem] h-[17.25rem]" />
-      <div className="absolute top-[90.13rem] left-[88.31rem] text-[3.75rem] font-semibold font-playfair-display text-dimgray text-left inline-block w-[1.94rem] h-[3.56rem]">{`>`}</div>
-      <div className="absolute top-[4.81rem] left-[83.75rem] bg-whitesmoke-400 w-[3.94rem] h-[204.19rem]" />
-      <div className="absolute top-[4.81rem] left-[0rem] bg-whitesmoke-400 w-[4rem] h-[204.19rem]" />
-      <img
-        className="absolute h-[0.88%] w-[2.35%] top-[23.17%] right-[39.13%] bottom-[75.95%] left-[58.52%] max-w-full overflow-hidden max-h-full"
-        alt=""
-        src="/vector9.svg"
-      />
-      <div className="absolute top-[70.19rem] left-[3.94rem] bg-gray-900 w-[79.81rem] h-[13.88rem]" />
-      <div className="absolute top-[73.13rem] left-[50.94rem] text-[1.25rem]">
-        HOME
-      </div>
-      <div className="absolute top-[73.13rem] left-[55.88rem] text-[1.25rem]">
-        CONTATO
-      </div>
-      <div className="absolute top-[73.13rem] left-[62.44rem] text-[1.25rem]">
-        SOBRE
-      </div>
-      <div className="absolute top-[73.13rem] left-[67.5rem] text-[1.25rem]">
-        NOSSA SOLUÇÕES
-      </div>
-      <div className="absolute top-[82.56rem] left-[33.19rem] text-[0.88rem] leading-[170%] font-be-vietnam-pro text-left">
-        © Copyright 2023 EP Energy Soluções em Energia
-      </div>
-      <div className="absolute top-[77.13rem] left-[33rem] text-[0.88rem] inline-block w-[19.06rem] h-[2.13rem]">
-        SAIBA MAIS EM NOSSAS REDES SOCIAIS
-      </div>
-      <div className="absolute top-[78.59rem] left-[34.34rem] box-border w-[16.44rem] h-[0.06rem] border-t-[1px] border-solid border-secondary" />
-      <img
-        className="absolute h-[2.12%] w-[2.64%] top-[94.23%] right-[52.89%] bottom-[3.65%] left-[44.48%] max-w-full overflow-hidden max-h-full"
-        alt=""
-        src="/vector5.svg"
-      />
-      <img
-        className="absolute h-[0.92%] w-[1.14%] top-[94.79%] right-[53.67%] bottom-[4.28%] left-[45.19%] max-w-full overflow-hidden max-h-full"
-        alt=""
-        src="/vector6.svg"
-      />
-      <img
-        className="absolute h-[2.12%] w-[1.35%] top-[94.23%] right-[50.11%] bottom-[3.65%] left-[48.54%] max-w-full overflow-hidden max-h-full"
-        alt=""
-        src="/vector7.svg"
-      />
-      <img
-        className="absolute h-[2.12%] w-[2.64%] top-[94.23%] right-[52.89%] bottom-[3.65%] left-[44.48%] max-w-full overflow-hidden max-h-full"
-        alt=""
-        src="/vector5.svg"
-      />
-      <img
-        className="absolute h-[0.92%] w-[1.14%] top-[94.79%] right-[53.67%] bottom-[4.28%] left-[45.19%] max-w-full overflow-hidden max-h-full"
-        alt=""
-        src="/vector6.svg"
-      />
-      <img
-        className="absolute h-[2.53%] w-[2.78%] top-[92.34%] right-[46.47%] bottom-[5.13%] left-[50.75%] max-w-full overflow-hidden max-h-full"
-        alt=""
-        src="/vector.svg"
-      />
-      <img
-        className="absolute h-[2.11%] w-[2.32%] top-[94.23%] right-[46.29%] bottom-[3.66%] left-[51.39%] max-w-full overflow-hidden max-h-full"
-        alt=""
-        src="/vector8.svg"
-      />
-      <div className="absolute top-[1.44rem] left-[19.75rem] inline-block w-[6.63rem]">
-        CONTATO
-      </div>
-      <div className="absolute top-[0.06rem] left-[68.63rem] bg-darkslategray-200 w-[19.06rem] h-[4.75rem]" />
-      <div className="absolute top-[1.38rem] left-[57.94rem] inline-block w-[6rem]">
-        SOBRE
-      </div>
-      <div className="absolute top-[9.38rem] left-[27.5rem] text-[2.5rem] font-semibold font-plus-jakarta-sans text-darkslategray-100 text-left inline-block w-[35.13rem]">
+      
+    <BarraNav/>  
+      
+      
+     
+      <div className="absolute top-[6.38rem] left-[38%] text-[1.7rem] font-semibold font-plus-jakarta-sans text-darkslategray-100 text-left inline-block w-[35.13rem]">
         Conhaça Nossas Soluções
       </div>
-      <div className="absolute top-[13.09rem] left-[16.34rem] box-border w-[55rem] h-[0.06rem] border-t-[1px] border-solid border-black" />
+      <div className="absolute top-[10.09rem] left-[16.34rem] box-border w-[55rem] h-[0.06rem] border-t-[1px] border-solid border-black" />
+
+
       <div className="absolute top-[13.25rem] left-[9.38rem] font-semibold font-playfair-display text-left inline-block w-[16.38rem] h-[2.63rem]">
         TRANSFORMADORES
       </div>
+
+      <a href="/desktop-26">
+        <img
+                  className="absolute top-[19rem] left-[10rem] rounded-12xs w-[16.63rem] h-[13rem] object-cover"
+                  alt=""
+                  src="/d53b9bf5ce7cc7b0f867e4a558dce38e-3@2x.png"
+        />
+       </a>
+
       <div className="absolute top-[31.75rem] left-[10.44rem] bg-steelblue w-[15.19rem] h-[3.56rem]" />
       <div className="absolute top-[31.75rem] left-[27.63rem] bg-steelblue w-[15.25rem] h-[3.56rem]" />
       <div className="absolute top-[32.88rem] left-[29.13rem] text-[1rem] font-playfair-display">
@@ -110,14 +45,10 @@ const NossasSolucoes: FunctionComponent = () => {
       <div className="absolute top-[32.31rem] left-[62.13rem] text-[1rem] font-playfair-display inline-block w-[14.81rem]">
         Painel de Distribuição de Baixa Tensão até 6300A
       </div>
-      <div className="absolute top-[1.38rem] left-[70.31rem] inline-block w-[13.44rem]">
-        NOSSAS SOLUÇÕES
-      </div>
-      <img
-        className="absolute top-[69.25rem] left-[10.44rem] w-[19.38rem] h-[11.63rem] object-cover"
-        alt=""
-        src="/customcolor-textlogoname-transparent-background-1@2x.png"
-      />
+
+      <Rodape/>
+      <MargensLaterais/>
+  
     </div>
   );
 };
